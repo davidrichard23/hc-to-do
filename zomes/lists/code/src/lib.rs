f125477c663522b2b3f40008e8164a30597dada6
davidRichard23 <d2391@me.com>
2019-03-12 22:43:06 -0400

    initial setup

---
+++ zomes/lists/code/src/lib.rs
@@ -0,0 +1,21 @@
+
+#[macro_use]
+extern crate hdk;
+ 
+define_zome! {
+    entries: [
+    ]
+ 
+    genesis: || {
+        Ok(())
+    }
+ 
+    functions: [
+    ]
+ 
+    traits: {
+    }
+}
+
+
+

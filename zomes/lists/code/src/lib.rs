9e7f982ef04ae0bc72d53e07965e9394a0e79a75
davidRichard23 <d2391@me.com>
2019-03-13 00:22:52 -0400

    create handler functions

--- zomes/lists/code/src/lib.rs
+++ zomes/lists/code/src/lib.rs
@@ -69,6 +69,62 @@ define_zome! {
 }
 
 
+fn handle_create_list(list: List) -> ZomeApiResult<Address> {
+    let list_entry = Entry::App(
+        "list".into(),
+        list.into()
+    );
+
+	hdk::commit_entry(&list_entry)
+}
+
+fn handle_add_item(list_item: ListItem, list_addr: HashString) -> ZomeApiResult<Address> {
+    let list_item_entry = Entry::App(
+        "listItem".into(),
+        list_item.into()
+    );
+
+	let item_addr = hdk::commit_entry(&list_item_entry)?;
+	hdk::link_entries(&list_addr, &item_addr, "items")?;
+	Ok(item_addr)
+}
+
+fn handle_get_list(list_addr: HashString) -> ZomeApiResult<GetListResponse> {
+
+    let list = get_as_type::<List>(list_addr.clone())?;
+    let list_items = hdk::get_links(&list_addr, "items")?.addresses()
+        .iter()
+        .map(|item_address| {
+            get_as_type::<ListItem>(item_address.to_owned())
+        })
+        .filter_map(Result::ok)
+        .collect::<Vec<ListItem>>();
+
+    Ok(GetListResponse{
+        name: list.name,
+        items: list_items
+    })
+}
+
+pub fn get_as_type<
+    R: TryFrom<AppEntryValue>
+> (address: HashString) -> ZomeApiResult<R> {
+    let get_result = hdk::get_entry(&address)?;
+    let entry = get_result.ok_or(ZomeApiError::Internal("No entry at this address".into()))?;
+    match entry {
+        Entry::App(_, entry_value) => {
+            R::try_from(entry_value.to_owned())
+                .map_err(|_| ZomeApiError::Internal(
+                    "Could not convert get_links result to requested type".to_string())
+                )
+        },
+        _ => Err(ZomeApiError::Internal(
+            "get_links did not return an app entry".to_string())
+        )
+    }
+}
+
+
 
 #[derive(Serialize, Deserialize, Debug, DefaultJson)]
 struct List {

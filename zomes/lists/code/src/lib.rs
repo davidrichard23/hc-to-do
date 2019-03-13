fd7e65ad4177db334c8f4978aec37a4b4b1c2f74
davidRichard23 <d2391@me.com>
2019-03-12 22:56:46 -0400

    add entry to zome definition

--- zomes/lists/code/src/lib.rs
+++ zomes/lists/code/src/lib.rs
@@ -1,9 +1,60 @@
 
 #[macro_use]
 extern crate hdk;
+    
+#![feature(try_from)]
+use std::convert::TryFrom;
+#[macro_use]
+extern crate hdk;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate holochain_core_types_derive;
+
+use hdk::{
+    error::{ZomeApiResult, ZomeApiError},
+    holochain_core_types::{
+        hash::HashString,
+        error::HolochainError,
+        dna::entry_types::Sharing,
+        json::JsonString,
+        cas::content::Address,
+        entry::{AppEntryValue, Entry},
+    }
+};
  
 define_zome! {
     entries: [
+        entry!(
+            name: "list",
+            description: "",
+            sharing: Sharing::Public,
+            native_type: List,
+            validation_package: || hdk::ValidationPackageDefinition::Entry,
+            validation: |list: List, _ctx: hdk::ValidationData| {
+                Ok(())
+            },
+            links: [
+                to!(
+                    "listItem",
+                    tag: "items",
+                    validation_package: || hdk::ValidationPackageDefinition::Entry,
+                    validation: |base: Address, target: Address, _ctx: hdk::ValidationData| {
+                        Ok(())
+                    }
+                )
+            ]
+        ),
+        entry!(
+            name: "listItem",
+            description: "",
+            sharing: Sharing::Public,
+            native_type: ListItem,
+            validation_package: || hdk::ValidationPackageDefinition::Entry,
+            validation: |list_item: ListItem, _ctx: hdk::ValidationData| {
+                Ok(())
+            }
+        )
     ]
  
     genesis: || {
@@ -34,4 +85,4 @@ struct ListItem {
 struct GetListResponse {
     name: String,
     items: Vec<ListItem>
-}
+}s
